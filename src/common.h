27b8e5f3b8c29c7312de7c47f2159b21d1b8f069
MoochMcGee <alegend45@gmail.com>
2015-08-31 12:50:33 -0500

    Initial commit

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <functional>
+#include "log.h"
+
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint32_t u32;
+typedef int32_t s32;
