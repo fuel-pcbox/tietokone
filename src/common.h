762409bf5b65bbc47e8dcf60770fd9eaf89f6ca3
MoochMcGee <alegend45@gmail.com>
2015-10-02 19:23:22 -0500

    Adding more

--- src/common.h
+++ src/common.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdio>
 #include <vector>
 #include <functional>
 #include "log.h"
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "common.h"
+
+int main(int ac, char** av)
+{
+    FILE* biosfp = fopen("roms/bios.bin");
+}
